4c93cc35eaf6c54673881c5bd37c1844a7c7d1c1
ayuknkem joshua <joesolo165@gmail.com>
2025-05-14 06:20:35 +0100

    ..

--- script .js
+++ script .js
@@ -1,10 +1,19 @@
 
-function min(a, b) {
-    return a < b ? a : b;
-  }
-  console.log(min(5, 10)); 
-let a =5;
-b =10;
+let colors =["red", "blue", "green", "yellow", "black", "orange"];
+let msg = document.getElementById("btn")
+let body = document.getElementById("col")
+ msg.addEventListener("click",myFunction);
+ function myFunction(){
+ let n = Math.floor(Math.random()*6)+1
+ body.style.background = colors[n];
+ }
 
+
+ let msg1 = document.getElementById("btn1")
+msg1.addEventListener("click", userFunction);
+function userFunction(){
+    colors.push(prompt("chose bacground color:"));
+    body.style.background = colors [colors.length-1];
+}
 
 
